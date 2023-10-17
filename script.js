d10e4aff747af0c6a3fed62987f4bfedc58e929c
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-10-17 12:04:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name=name;
-		this.age=age;
+		this._name=name;
+		this._age=age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
 	get age(){
-		return this.age;
+		return this._age;
 	}
 	
 }
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studying`)
+		console.log(`${this._name} is studying`)
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching`)
+		console.log(`${this._name} is teaching`)
 	}
 }
 
